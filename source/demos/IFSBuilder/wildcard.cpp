f02b969b462f0b0ead567d39207140cb494fe53b
morgan3d <morgan3d>
2003-04-04 16:12:27 +0000

    *** empty log message ***

---
+++ source/demos/IFSBuilder/wildcard.cpp
@@ -0,0 +1,111 @@
+/**
+  @file IFSBuilder/wildcard.cpp
+
+  Code for handling filenames with wild cards.
+
+  @maintainer Morgan McGuire, matrix@graphics3d.com
+
+  @created 2002-04-04
+  @edited  2002-04-04
+ */ 
+
+#include <G3DAll.h>
+
+bool hasWildcard(
+    std::string str) {
+
+    return
+        (str.find('*') != std::string::npos) ||
+        (str.find('?') != std::string::npos);
+}
+
+
+std::string getFilename(
+    const std::string& filename) {
+
+    // Find the last slash
+    int slIndex = filename.rfind("\\");
+    int tmp = filename.rfind("/");
+    if (tmp > slIndex) {
+        slIndex = tmp;
+    }
+
+    int extIndex = filename.rfind(".");
+    if (extIndex == std::string::npos) {
+        extIndex = filename.length();
+    }
+
+    std::string result;
+    result.assign(filename, slIndex + 1, extIndex - slIndex - 1);
+
+    return result;
+}
+
+
+std::string getPath(
+    const std::string& filename) {
+
+    // Find the last slash
+    int slIndex = filename.rfind("\\");
+    int tmp = filename.rfind("/");
+    if (tmp > slIndex) {
+        slIndex = tmp;
+    }
+
+    std::string result;
+    if (slIndex == -1) {
+        result = "./";
+    } else {
+        result.assign(filename, 0, slIndex + 1);
+    }
+
+    return result;
+}
+
+
+std::string getExtension(
+    const std::string &x) {
+
+	// Search backwards for the period
+	int i = x.length() - 1;
+	while ((i > 0) && (x.at(i) != '.')) {
+		i--;
+	}
+
+	// Return everything to the right of the period.
+	return (std::string)(x.c_str() + (i + 1));
+}
+
+
+void expandWildcard(
+    const std::string& str,
+    Array<std::string>& result) {
+
+#ifdef WIN32
+    if (hasWildcard(str)) {
+
+        //  Separate the path and wildcard
+        std::string path = getPath(str);
+
+        WIN32_FIND_DATA foundFile;
+        HANDLE searchHandle = FindFirstFile(str.c_str(), &foundFile);
+
+        if (searchHandle != INVALID_HANDLE_VALUE) {
+            do {
+                std::string inFile = foundFile.cFileName;
+                result.append(path + inFile);
+            } while (FindNextFile(searchHandle, &foundFile));
+
+            FindClose(searchHandle);
+        }
+    } else {
+        result.append(str);
+    }
+
+#else // !WIN32
+
+    result.append(str);
+
+#endif // WIN32
+
+}
