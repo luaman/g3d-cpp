7d274a5e905b1f3a219d514dda56496465be022b
morgan3d <morgan3d>
2003-07-21 23:45:07 +0000

    fixed directories

--- build.py
+++ build.py
@@ -106,14 +106,14 @@ def lib():
         # Copy the lib's to the right directory
         if (x == 0):
             copyIfNewer("temp/debug/g3d/.libs/libG3D_debug.a",            installDir + "/lib/libG3D_debug.a")
-            copyIfNewer("temp/debug/g3d/.libs/libG3D_debug.so.0.0.0",     installDir + "/lib/libG3D_debug.so.0")
+            copyIfNewer("temp/debug/g3d/.libs/libG3D_debug.so.0.0.0",     installDir + "/lib/libG3D_debug.so")
             copyIfNewer("temp/release/g3d/.libs/libG3D.a",                installDir + "/lib/libG3D.a")
-            copyIfNewer("temp/release/g3d/.libs/libG3D.so.0.0.0",         installDir + "/lib/libG3D.so.0")
+            copyIfNewer("temp/release/g3d/.libs/libG3D.so.0.0.0",         installDir + "/lib/libG3D.so")
 
             copyIfNewer("temp/debug/glg3d/.libs/libGLG3D_debug.a",        installDir + "/lib/libGLG3D_debug.a")
-            copyIfNewer("temp/debug/glg3d/.libs/libGLG3D_debug.so.0.0.0", installDir + "/lib/libGLG3D_debug.so.0")
+            copyIfNewer("temp/debug/glg3d/.libs/libGLG3D_debug.so.0.0.0", installDir + "/lib/libGLG3D_debug.so")
             copyIfNewer("temp/release/glg3d/.libs/libGLG3D.a",            installDir + "/lib/libGLG3D.a")
-            copyIfNewer("temp/release/glg3d/.libs/libGLG3D.so.0.0.0",     installDir + "/lib/libGLG3D.so.0")
+            copyIfNewer("temp/release/glg3d/.libs/libGLG3D.so.0.0.0",     installDir + "/lib/libGLG3D.so")
 
     if (x != 0):
         print "*** Errors encountered during compilation.  Build process halted."
