72bf61571967e0d982ded912b60dfb3252c2911c
morgan3d <morgan3d>
2003-10-30 17:39:34 +0000

    error faq

--- source/include/G3D/mainpage.h
+++ source/include/G3D/mainpage.h
@@ -8,7 +8,8 @@
 /**
  @mainpage
    <P>
-    <CENTER><A HREF="http://sourceforge.net/forum/forum.php?forum_id=262426">User Forum</A> 
+    <CENTER><A HREF="http://sourceforge.net/forum/?group_id=76879">Forums</A> 
+	| <A HREF="error.html">Error FAQ</A> 
     |   \ref Introduction  |  \ref Installation  |
     \ref defines "\#defines"  |  \ref License  |  \ref Contributors   |
     <A HREF="changelog.html">Change Log</A></CENTER>
@@ -336,105 +337,7 @@
   (as headers and a windows binary).  If you need zlib for your own platform 
   or wish to build it yourself, go to http://www.gzip.org/zlib/.
 
-  <H3>Error FAQ</H3>
-   There are a few misleading error messages you can run into.  Here are the 
-   common fixes.
-
-   <P>
-   <PRE><B>
-msvcprtd.lib(MSVCP60D.dll) : error LNK2005: "public: __thiscall std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> >::~basic_string<char,struct std::char_traits<char>,class std::allocator<char> >(void)" (??1?$basic_strin
-g@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@QAE@XZ) already defined in main.obj
-msvcprtd.lib(MSVCP60D.dll) : error LNK2005: "public: __thiscall std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> >::basic_string<char,struct std::char_traits<char>,class std::allocator<char> >(char const *,class basic_
-string<char,struct std::char_traits<char>,class std::allocator<char> >::allocator<char> const &)" (??0?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@QAE@PBDABV?$allocator@D@1@@Z) already defined in main.obj
-msvcprtd.lib(MSVCP60D.dll) : error LNK2005: "public: char const * __thiscall std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> >::c_str(void)const " (?c_str@?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@Q
-BEPBDXZ) already defined in main.obj
-msvcprtd.lib(MSVCP60D.dll) : error LNK2005: "public: class std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> > & __thiscall std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> >::operator=(cl
-ass std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> > const &)" (??4?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@QAEAAV01@ABV01@@Z) already defined in main.obj
-msvcprtd.lib(MSVCP60D.dll) : error LNK2005: "public: __thiscall std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> >::basic_string<char,struct std::char_traits<char>,class std::allocator<char> >(class basic_string<char,s
-truct std::char_traits<char>,class std::allocator<char> >::basic_string<char,struct std::char_traits<char>,class std::allocator<char> > const &)" (??0?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@QAE@ABV01@@Z) already defined in main.
-obj
-msvcprtd.lib(MSVCP60D.dll) : error LNK2005: "public: static double __cdecl std::numeric_limits<double>::infinity(void)" (?infinity@?$numeric_limits@N@std@@SANXZ) already defined in main.obj
-msvcprtd.lib(MSVCP60D.dll) : error LNK2005: "public: static double __cdecl std::numeric_limits<double>::quiet_NaN(void)" (?quiet_NaN@?$numeric_limits@N@std@@SANXZ) already defined in main.obj
-msvcprtd.lib(MSVCP60D.dll) : error LNK2005: "public: unsigned int __thiscall std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> >::size(void)const " (?size@?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@QBE
-IXZ) already defined in main.obj
-msvcprtd.lib(MSVCP60D.dll) : error LNK2005: "public: class std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> > & __thiscall std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> >::operator+=(c
-lass std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> > const &)" (??Y?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@QAEAAV01@ABV01@@Z) already defined in main.obj
-msvcprtd.lib(MSVCP60D.dll) : error LNK2005: "bool __cdecl std::operator==(class std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> > const &,char const *)" (??8std@@YA_NABV?$basic_string@DU?$char_traits@D@std@@V?$allocat
-or@D@2@@0@PBD@Z) already defined in main.obj
-msvcprtd.lib(MSVCP60D.dll) : error LNK2005: "public: class std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> > & __thiscall std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> >::operator+=(c
-har const *)" (??Y?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@QAEAAV01@PBD@Z) already defined in main.obj
-MSVCRTD.lib(MSVCRTD.dll) : error LNK2005: _fflush already defined in LIBCD.lib(fflush.obj)
-MSVCRTD.lib(MSVCRTD.dll) : error LNK2005: _fclose already defined in LIBCD.lib(fclose.obj)
-MSVCRTD.lib(MSVCRTD.dll) : error LNK2005: _free already defined in LIBCD.lib(dbgheap.obj)
-MSVCRTD.lib(MSVCRTD.dll) : error LNK2005: _realloc already defined in LIBCD.lib(dbgheap.obj)
-MSVCRTD.lib(MSVCRTD.dll) : error LNK2005: __vsnprintf already defined in LIBCD.lib(vsnprint.obj)
-MSVCRTD.lib(MSVCRTD.dll) : error LNK2005: _exit already defined in LIBCD.lib(crt0dat.obj)
-MSVCRTD.lib(MSVCRTD.dll) : error LNK2005: _malloc already defined in LIBCD.lib(dbgheap.obj)
-MSVCRTD.lib(MSVCRTD.dll) : error LNK2005: __CrtIsValidHeapPointer already defined in LIBCD.lib(dbgheap.obj)
-MSVCRTD.lib(MSVCRTD.dll) : error LNK2005: __CrtIsValidPointer already defined in LIBCD.lib(dbgheap.obj)
-MSVCRTD.lib(MSVCRTD.dll) : error LNK2005: _sprintf already defined in LIBCD.lib(sprintf.obj)
-MSVCRTD.lib(MSVCRTD.dll) : error LNK2005: _toupper already defined in LIBCD.lib(toupper.obj)
-MSVCRTD.lib(MSVCRTD.dll) : error LNK2005: _tolower already defined in LIBCD.lib(tolower.obj)
-MSVCRTD.lib(MSVCRTD.dll) : error LNK2005: _calloc already defined in LIBCD.lib(dbgheap.obj)
-MSVCRTD.lib(MSVCRTD.dll) : error LNK2005: _strncpy already defined in LIBCD.lib(strncpy.obj)
-MSVCRTD.lib(MSVCRTD.dll) : error LNK2005: __isctype already defined in LIBCD.lib(isctype.obj)
-LINK : warning LNK4098: defaultlib "MSVCRTD" conflicts with use of other libs; use /NODEFAULTLIB:library
-Debug/GLG3D_Demo.exe : fatal error LNK1169: one or more multiply defined symbols found
-</PRE></B>
-   <BR>
-   You are linking against the wrong standard library.  Navigate the MSVC menus to
-   Project:Settings:C++:Code Generation and select 
-   "Debug Multithreaded DLL" for your debug build and "Multithreaded DLL" for
-   your release build.
-   <P>
-
-  <B>Application Error: The application failed to initialize properly (0xc0000022). Click on OK to terminate the application.</B>
-  <BR>
-   At runtime, your program can't load the SDL.dll, zlib.dll, or glut32.dll dynamic libraries.
-   This is probably occurring because the current working directory is invalid or because
-   the files and directory exist but cannot be accessed.  This frequently occurs when a Windows
-   and Linux file system don't get along-- we've seen the problem occur under
-   Cygwin when permissions are not set to 777 and on Samba shares.
-
-   <P>
-
-   <B>../include/G3D/g3dmath.h:27: limits: No such file or directory</B>
-   <BR>You are using an old version of GCC on Linux. Add these lines to your .cshrc:<br>
-   <ul>
-   <li> setenv CC gcc-3.2
-   <li> setenv CXX g++-3.2
-   </ul>
-
-   <P>
-   <B>array.h(233) : error C2512: 'X' : no appropriate default
-   constructor available <BR>array.h(195) : while compiling
-   class-template member function 'void __thiscall G3D::Array<class
-   X>::resize(int,bool)'</B> <BR> You created a G3D::Array of a class
-   that does not provide a default constructor (constructor with no
-   arguments).  Define a default constructor for your class--
-   G3D::Array needs it to initialize new elements when you invoke
-   G3D::Array::resize().
-
-    <P> <B>sdlmain.lib(SDL_main.obj) : error LNK2005: _main already
-   defined in main.obj</B> <BR>You need to \#include <SDL.h> in your
-   main.cpp file.
-
-   <P>
-   <B>DemoSettings.obj : error LNK2001: unresolved external symbol _glActiveTextureARB</B>
-   <BR>Some other (e.g. MSVC .NET) program's include directory is listed before the G3D directory
-   and contains conflicting OpenGL drivers.
-   <P>
-
-   <B>main.cpp(9) : fatal error C1083: Cannot open include file:
-   'G3DAll.h': No such file or directory</B> <BR> You need to add the
-   graphics3d/include directory to your include path (instructions
-   above)
-
-   <B>LINK : fatal error LNK1181: cannot open input file
-   "glg3d.lib"</B> <BR> You need to add the graphics3d/lib directory
-   to your library path (instructions above)
-
-   <H3>Directories</H3>
+  <H3>Directories</H3>
    The distribution contains the following subdirectories:
 
     <UL>
@@ -452,10 +355,11 @@ Debug/GLG3D_Demo.exe : fatal error LNK1169: one or more multiply defined symbols
 
     </UL>
    
+
    <H3>Data</H3>
    <P>
 
-   Standard (public domain) 2D and 3D data are provided with this
+   Standard (and mostly public domain) 2D and 3D data are provided with this
    library.  These may be useful as test data or for comparison with
    previously published results.
    
