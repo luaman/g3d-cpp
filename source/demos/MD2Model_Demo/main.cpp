22fc2caa7159db3993f3b9ecc1feb20431756768
morgan3d <morgan3d>
2004-01-07 06:20:40 +0000

    *** empty log message ***

--- source/demos/MD2Model_Demo/main.cpp
+++ source/demos/MD2Model_Demo/main.cpp
@@ -68,9 +68,6 @@ int main(int argc, char** argv) {
 
     renderDevice->resetState();
 	renderDevice->setColorClearValue(Color3(1, 1, 1));
-    float spec[] = {0.1f, 0.1f, 0.1f, 1.0f};
-    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, spec);
-    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 60);
 
     getModelNameArray();
 
