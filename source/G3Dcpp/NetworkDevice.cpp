c4da742eeec748e7fd52c0bdf4c07f25916daf7c
morgan3d <morgan3d>
2004-03-29 19:03:27 +0000

    Linux host name

--- source/G3Dcpp/NetworkDevice.cpp
+++ source/G3Dcpp/NetworkDevice.cpp
@@ -296,7 +296,22 @@ NetworkDevice::NetworkDevice() {
 
 
 std::string NetworkDevice::localHostName() const {
-    struct hostent* host = gethostbyname("localhost");
+
+    struct hostent* host = NULL;
+
+    /*    #ifdef G3D_WIN32
+        // On Windows, this returns the machine name.  On Linux
+        // it would just parrot back 'localhost'.
+        host = gethostbyname("localhost");
+    #else
+    */
+        char ac[128];
+        if (gethostname(ac, sizeof(ac)) == -1) {
+            if (debugLog) {debugLog->printf("Error while getting local host name\n");}
+            return "localhost";
+        }
+        host = gethostbyname(ac);
+        // #endif
     return host->h_name;
 }
 
