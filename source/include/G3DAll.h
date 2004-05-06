fdacf48cc50a164d62e231fa6f427fc7cea3607a
giuliom <giuliom>
2004-05-06 17:37:19 +0000

    Added comment to justify the additional 'using' statements

--- source/include/G3DAll.h
+++ source/include/G3DAll.h
@@ -23,7 +23,12 @@
 #include "GLG3D.h"
 
 using namespace G3D;
-using G3D::Texture;
+// The following statements should be not be necessary. 
+// However, we experienced problems under MSVC 6.00 SP4 
+// when compiling against certain code in its own 
+// namespace-- the 'using namespace G3D' failed to bring some 
+// of the names, like Texture, into the global namespace.
+using G3D::Texture;         
 using G3D::TextureRef;
 using G3D::RenderDevice;
 using G3D::AMPM;
