9a9ebe4c80c0587d813bdcbb2cec0b04dde828ed
morgan3d <morgan3d>
2004-09-11 15:13:08 +0000

    debug

--- source/docsource/guide-debug.h
+++ source/docsource/guide-debug.h
@@ -13,7 +13,6 @@ Application Framework <IMG SRC="forwardarrow.gif" BORDER=0 ALIGN=MIDDLE></A></I>
     G3D provides many services to help you find bugs in your own code.
 
 
- 	(show debug dialog)
 	Debug and release builds are different!
 	
     Use the debug binaries with your debug build (and build them yourself if you want to step into the G3D source code.)
@@ -22,7 +21,21 @@ Application Framework <IMG SRC="forwardarrow.gif" BORDER=0 ALIGN=MIDDLE></A></I>
     the debug target of your project-- many errors in your code will be caught in debug mode by 
     %G3D.
 
-	G3D::debugAssert, G3D::debugAssertM, G3D::alwaysAssertM, G3D::debugAssertGLOk
+
+    %G3D provides assertion statements that are more powerful than those included with
+    the C runtime.  They break at the line of your code where the assertion was violated
+    (instead of inside the assert code) and have a more useful dialog than the Windows'
+    "Abort, Retry, Ignore".
+
+    G3D::debugAssert, G3D::debugAssertM, G3D::alwaysAssertM, G3D::debugAssertGLOk
+
+    A prompt will appear when an assertion is violated.  On Linux the prompt will
+    appear at the command line.  On Windows and OS X it will be a GUI dialog.
+ 	<IMG SRC="debug.jpg">
+
+    Note that you can choose to ignore the violation and continue your program.
+
+
 	G3D::getOpenGLState
 	G3D::Log
 	Debugging shaders
