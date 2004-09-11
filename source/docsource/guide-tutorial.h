424abe1d6138c4eb51c3e005413c7fa5afa59a98
sbshine <sbshine>
2004-09-11 18:32:55 +0000

    Made changes requested in https://sourceforge.net/tracker/?func=detail&atid=548565&aid=1026208&group_id=76879: - replaced fairy model in screenshot with hunter - removed references to assignment and course - used %G3D instead of G3D or g3d to prevent doxygen from going too crazy with hyperlinks - removed "If you're doing this tutorial..." section - fixed link for installation instructions - added screenshot of the .NET configuration dialog - removed onwards and upwards section

--- source/docsource/guide-tutorial.h
+++ source/docsource/guide-tutorial.h
@@ -13,45 +13,43 @@ Debugging <IMG SRC="forwardarrow.gif" BORDER=0 ALIGN=MIDDLE></A></I></FONT></TD>
 
 <img src="splash.jpg" width=600></img>
 
-The goal of this assignment is to set up, compile, run and debug a 3D scene
-using the G3D library. This handout walks you through the process, but it
-intentionally written to force you to refer to the online documentation available
-on the course web page. Except for maybe the very last part of the assignment,
-you shouldn't have to do any math, just a lot of reading.
+The goal of this tutorial is to set up, compile, run and debug a 3D scene
+using the %G3D library. This handout walks you through the process, but it
+intentionally written to force you to refer to the online documentation: \link indexedbytopic \endlink. 
 
-You should make extensive use of the G3D documentation at:
-http://g3d-cpp.sourceforge.net/html/index.html 
-(and also in your local G3D installation, in html/index.html in your G3D installation.)
+Except for maybe the very last part of the tutorial,
+you shouldn't have to do any math, just a lot of reading, thinking, and coding.
 
-That index page lists all of the classes in G3D by functionality. It has links
+You should make extensive use of the %G3D documentation at:
+http://g3d-cpp.sourceforge.net/html/index.html and <a href="index.html">as part of your %G3D install</a>.
+
+That index page lists all of the classes in %G3D by functionality. It has links
 to alphabetic lists as well. Look at <a href="map.png">the Namespace Map</a> 
-for an architectural view of G3D. You may also want to look at the OpenGL
-man pages because many G3D calls are designed to be similar to an equivalent
+for an architectural view of %G3D. You may also want to look at the OpenGL
+man pages because many %G3D calls are designed to be similar to an equivalent
 OpenGL call. Use either man or:
 http://www.3dlabs.com/support/developer/GLmanpages/
 
-G3D also includes some source code demos that may help if you need to brush
-up on your OpenGL-style rendering. See the demos directory of your G3D installation.
+%G3D also includes some source code demos that may help if you need to brush
+up on your OpenGL-style rendering. See the demos directory of your %G3D installation.
 The GLG3D_Demo will be particularly useful to you at this stage.
 
-If you're doing this tutorial as the first assignment in a class, you can bet that 
-future assignments will assume that you are familiar with the G3D library
-and can create 3D scenes, detect collisions, and handle user input. With a solid 
-grasp of G3d, you won't have to learn new support code for each project and 
-will have a powerful, reliable base of code on which to build).
+We encourage you to work through this tutorial as a first step in learning this package. 
+With a solid command of %G3D, you will have a powerful, reliable base of code on which to build).
 
 \section prereqs Prerequisites
 
 To make best use of this tutorial, we recommend that you are already comfortable with 
 <ul>
+<li>Coordinates in 3D</li>
 <li>C++</li>
 <li>C++ templates (you can fake it if you don't know this will yet)</li>
 <li>Compiling and running c++ projects on the architecture of your choice</li>
 <li>OpenGL: you don't need to be an expert, but you should feel okay about setting up lighting and drawing primitives</li>
 </ul>
-In addition, you should have a complete and working installation of G3D, including
-the packages it requires. See \link guideinstall \endlink for installation instructions. The
-directory in which you installed G3D will be referred to as G3D_ROOT for the duration of this tutorial. 
+In addition, you should have a complete and working installation of %G3D, including
+the packages it requires. See \link guideinstall the installation instructions \endlink. The
+directory in which you installed %G3D will be referred to as G3D_ROOT for the duration of this tutorial. 
 
 You will need the following packages and tools installed. See your teaching assistant
 or sysadmin if you have problems installing or locating these tools. 
@@ -97,14 +95,17 @@ CXX g++-3.2
 		
 	@subsection windowsprojsetup Windows Project Setup
 
-Even if your course requires you to hand in code that compiles and runs correctly on
+Even if your end goal is code that compiles and runs correctly on
 Linux, you may want to use Windows to develop your code because of its more
-featureful development environment. G3D works equally well, if not better, under
+featureful development environment. %G3D works equally well, if not better, under
 MSVC: the library integrates into the IDE, it runs faster, and you get the nicer
 MSVC debugger.
 
 Copy SDL.dll, glut32.dll, and zlib1.dll from G3D_ROOT/libraries/g3d/demos
-to your project directory. You must do this for every G3D project. 
+to your project directory. You must do this for every %G3D project. Then, do
+one of the next two steps, depending on whether you're using Visual C++ 6.0, or 
+Visual Studio .NET 2003. (If you're using another IDE, adapt these instructions to 
+meet your needs.)
 
 @subsubsection vc6 Microsoft Visual C++ 6.0
 
@@ -115,18 +116,20 @@ Application" template. Give the project a name.
 Next, you need to add two additional paths to the project options for libraries
 and include files. Bring up the "Option" window from the "Tools" menu. Select
 the "Include files" item in the "Show directories for:" drop-down list, and add
-two new entries for the SDL and G3D libraries. If you have used default installation locations,
+two new entries for the SDL and %G3D libraries. If you have used default installation locations,
 these will be c:/SDL-1.2.7 and G3D_ROOT/win32-lib.
 Next, select the "Include files" item under the "Show directories for:" drop-down list, 
-and add the two new paths or SDL and G3D.
+and add the two new paths or SDL and %G3D.
 
 @subsubsection vc7 Microsoft Visual Studio .NET 2003
 
 Go to Tools:Options. Select the Projects heading, and within that heading, Visual C++
-Directories. In that panel, select "Show Directories For: Include files." Add the g3d
+Directories. In that panel, select "Show Directories For: Include files." Add the %G3D
 include directory, G3D_ROOT/include, and the SDL include directory, probably C:/SDL-1.2.7/include
 In the same panel, select "Show Directories For: Library Files."
-Add the g3d library directory, G3D_ROOT/win32-7-lib.
+Add the %G3D library directory, G3D_ROOT/win32-7-lib.
+
+<img src="msvsnet2003-screenshot.png" align=LEFT>
 
 Make a new, empty Win32 Project. (File -> New -> Project, select
 Visual C++ Project, then "Win32 Project". Give it a name, and click okay.
@@ -181,19 +184,19 @@ class App: public GApp {
 
 @subsection more Filling out the new project
 
-<i>Note about namespaces:</i> All G3D classes are defined in the namespace G3D,
+<i>Note about namespaces:</i> All %G3D classes are defined in the namespace G3D,
 so we'll explicitly include that in this handout (ie: G3D::GApp). Nevertheless,
 the G3DAll.h file includes the line using namespace G3D so you won't have to
 explicitly type the namespace in your program.
 
-Next, define a class called <tt>Demo</tt> that inherits from <tt>G3D::GApplet</tt>. The constructor
+Next, define a class called <tt>Demo</tt> that inherits from G3D::GApplet. The constructor
 should take a <tt>App*</tt> as a parameter and pass it to the parent's contructor.
 Your <tt>Demo</tt> class should store the passed-in <tt>App*</tt> in a member variable of its own.
-Override the <tt>G3D::GApplet::doGraphics</tt> method to clear the screen by calling
-the <tt>G3D::RenderDevice::clear</tt> method on your app's renderDevice. Set
+Override the G3D::GApplet::doGraphics method to clear the screen by calling
+the G3D::RenderDevice::clear method on your app's renderDevice. Set
 the projection and camera matrices from your app's <tt>debugCamera</tt> using the
-<tt>G3D::RenderDevice::setProjectionAndCameraMatrix</tt> method. To get an idea
-of there the origin lies, render a default set of axes using <tt>G3D::Draw::axes</tt>
+G3D::RenderDevice::setProjectionAndCameraMatrix method. To get an idea
+of there the origin lies, render a default set of axes using G3D::Draw::axes
 Compile and run the program with <tt>compile --run</tt> on linux, or <tt>F5</tt> on windows. 
 You should see a gray background with a set of colored axes in the middle. (You may have to move
 the mouse around to see the axes.)
@@ -202,7 +205,7 @@ In debug mode, you get a few things for free. For example, you can control the
 camera with standard first-person game controls:
 <ul>
 <li>Use the mouse to tilt the camera sideways and up/down</li>
-<li>Hold theWkey to move the camera forward, the S key to move it backward</li>
+<li>Hold the W key to move the camera forward, the S key to move it backward</li>
 <li>Hold the A and D keys to translate the camera left and right</li>
 <li>Press the Esc key to quit the program</li>
 </ul>
@@ -219,7 +222,7 @@ to slow things down to something manageable.
 
 Next, we are going to add a sky and lighting to the scene. Add a member
 variable of type G3D::SkyRef (not SkyRef*) to your <tt>Demo</tt> class. By the naming
-conventions used across G3D, any class that ends with the Ref suffix is a
+conventions used across %G3D, any class that ends with the Ref suffix is a
 reference-counted pointer: use it as a pointer (i.e. use -> or the * operator),
 but do not delete it explicitly.
 Override the G3D::GApplet::init method in your <tt>Demo</tt> class, and have your
@@ -265,7 +268,7 @@ G3D::TextureRef to the Demo class and initialize the texture in your Demo::init
 method, using G3D::Texture::fromFile. You can use any .JPG, .BMP, .TGA
 or .PCX ¯le you like for this. Modify your rendering code in doGraphics to
 assign this texture to texture unit 0, and supply appropriate texture coordinates
-using G3D::RenderDevice. Again, the G3D calls correspond directly to
+using G3D::RenderDevice. Again, the %G3D calls correspond directly to
 OpenGL calls.
 
 It will be easier to debug the texture coordinates if you change the default camera
@@ -274,7 +277,7 @@ by calling G3D::GCamera methods on your app's <tt>debugCamera</tt>.
 
 @section scenegraph Create a Scene Graph
 
-G3D is low level and does not provide a scene graph structure. You must make
+%G3D is low level and does not provide a scene graph structure. You must make
 your own based on the needs of your project. Fortunately, the data structures
 and rendering support provided are powerful enough that it is easy to make a
 scene graph.
@@ -367,17 +370,6 @@ G3D::RenderDevice::configureReflectionMap)</li>
 <a href="http://www.flipcode.com/articles/article_objectoutline.shtml">hardware accelerated
 object outlining technique by Max McGuire</a>. G3D::RenderDevice has several 
 methods that support the stencil buffer.</li>
-
-@section onwards Onwards and Upwards
-
-Now that you've gotten your feet wet with G3D, here are some more resources that 
-you might find useful:
-<ul>
-<li><a href="http://sourceforge.net/projects/g3d-cpp/">SourceForge g3d project</a>, including
-<a href="http://sourceforge.net/forum/?group_id=76879">forums</a> and a <a href="http://sourceforge.net/tracker/?group_id=76879&atid=548562">bugtracker</a>.
-<li>The \link guidewelcome Manual \endlink goes into more detail on particularly interesting parts of G3D. 
-<li>The <tt>contrib</tt> directory of your G3D installation contains interesting/useful examples and tools.</li>
-</ul>
 
 Tutorial questions, comments, feedback? Mail to sbshine at users.sourceforge.net. 
  */
