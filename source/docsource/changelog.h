f78ca760b36cc79baa386e8378f25a5d514b5ee1
vejita <vejita>
2004-10-19 19:36:39 +0000

    Sky::fromCubeMap and iCompile compiling.

--- source/docsource/changelog.h
+++ source/docsource/changelog.h
@@ -22,6 +22,8 @@
      <LI> G3d::IFSModel now loads IFS 1.1 [Peter]
      <LI> G3d::IFSModel now loads and saves PLY2 files (plain text IFS format) [Peter]
      <LI> Automatically switch to glCompressedTexImage2D in G3d::Texture::fromMemory [Corey]
+     <LI> Added Sky::fromCubeMap for preloaded CubeMap TextureRef's [Corey]
+     <LI> Demo and Test projects now build with iCompile which is included [Corey]
      <LI> Fix: GWindows now center and maximize on the primary monitor for Windows.
      <LI> Fix: [ 1049674 ] TextInput failes on X. numbers.
      <LI> Fix: [ 1044028 ] Linux TextOutput Warning
