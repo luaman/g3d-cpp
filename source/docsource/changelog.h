0f4e92688baefd806f4a9338cac6f613f03c325f
vejita <vejita>
2005-03-21 19:33:42 +0000

    no message

--- source/docsource/changelog.h
+++ source/docsource/changelog.h
@@ -152,6 +152,8 @@
      <LI> Added G3D::Sky::fromCubeMap for preloaded CubeMap TextureRef's [Corey]
      <LI> Added G3D::Sky::fromFile and deprecated Sky::create [Corey]
      <LI> Demo and Test projects now build with iCompile, which is included [Corey]
+     <LI> Fix: Matrix4 operator[] was returning a matrix value cast to a pointer [Corey]
+     <LI> Fix: Matrix3 and Matrix4 had missing float* / const float* operators [Corey]
      <LI> Fix: Rect2D::clip broken for types other than Vector2
      <LI> Fix: RenderDevice::configureShadowMap result depends on objectToWorldMatrix
      <LI> Fix: [ 1150650 ] DebugBreak() undefined
