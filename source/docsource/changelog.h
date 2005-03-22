ffc0d31f497f0da6afd71e1f670d9fcca09accf9
vejita <vejita>
2005-03-22 16:06:32 +0000

    *** empty log message ***

--- source/docsource/changelog.h
+++ source/docsource/changelog.h
@@ -152,6 +152,7 @@
      <LI> Added G3D::Sky::fromCubeMap for preloaded CubeMap TextureRef's [Corey]
      <LI> Added G3D::Sky::fromFile and deprecated Sky::create [Corey]
      <LI> Demo and Test projects now build with iCompile, which is included [Corey]
+     <LI> Fix: [ 1032742 ] OS X _DEBUG not defined [Derek]
      <LI> Fix: 16-bit integer reads in BinaryInput that always reversed endianness. (OSX file reading) [Corey + Derek]
      <LI> Fix: Matrix4 operator[] was returning a matrix value cast to a pointer [Corey]
      <LI> Fix: Matrix3 and Matrix4 had missing float* / const float* operators [Corey]
