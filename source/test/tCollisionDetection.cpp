502f79bb6d6a45e01f79eb099336916612f6047b
morgan3d <morgan3d>
2005-07-08 02:19:37 +0000

    *** empty log message ***

---
+++ source/test/tCollisionDetection.cpp
@@ -0,0 +1,99 @@
+#include "../include/G3DAll.h"
+
+void testCollisionDetection() {
+    printf("CollisionDetection ");
+
+
+    {
+        Sphere s(Vector3(0,1,0), 1);
+        Plane  p = Plane::fromEquation(0,1,0,0);
+        Array<Vector3> contact;
+        Array<Vector3> outNormal;
+
+        float depth = CollisionDetection::penetrationDepthForFixedSphereFixedPlane(
+            s, p, contact, outNormal);
+
+        debugAssert(outNormal[0] == -p.normal());
+        debugAssert(depth == 0);
+
+        s = Sphere(Vector3(0,2,0), 1);
+
+        depth = CollisionDetection::penetrationDepthForFixedSphereFixedPlane(
+            s, p, contact, outNormal);
+
+        debugAssert(depth < 0);
+    }
+
+    {
+        Sphere s(Vector3(0,1,0), 1);
+        Sphere r(Vector3(0,-.5,0), 1);
+        Array<Vector3> contact;
+        Array<Vector3> outNormal;
+
+        float depth = CollisionDetection::penetrationDepthForFixedSphereFixedSphere(
+            s, r, contact, outNormal);
+        (void)depth;
+
+        debugAssert(outNormal[0] == Vector3(0,-1,0));
+        debugAssert(depth == .5);
+    }
+
+    {
+        Sphere s(Vector3(5, 0, 0), 1);
+        Sphere f(Vector3(4.5, 0, 0), 1);
+        debugAssert(CollisionDetection::fixedSolidSphereIntersectsFixedSolidSphere(s, f));
+    }
+
+    {
+        Sphere s(Vector3(5, 0, 0), 1);
+        Sphere f(Vector3(1, 0, 0), 2);
+        debugAssert(! CollisionDetection::fixedSolidSphereIntersectsFixedSolidSphere(s, f));
+    }
+
+    {
+        Sphere s(Vector3(5, 0, 0), 1);
+        Box    f(Vector3(-1, -1.5, -2), Vector3(4.5, 2, 8));
+        debugAssert(CollisionDetection::fixedSolidSphereIntersectsFixedSolidBox(s, f));
+    }
+
+    {
+        Sphere s(Vector3(0, 0, 0), .1);
+        Box    f(Vector3(-1, -1.5, -2), Vector3(4.5, 2, 8));
+        debugAssert(CollisionDetection::fixedSolidSphereIntersectsFixedSolidBox(s, f));
+    }
+
+    {
+        Sphere s(Vector3(6, 0, 0), 1);
+        Box    f(Vector3(-1, -1.5, -2), Vector3(4.5, 2, 8));
+        debugAssert(! CollisionDetection::fixedSolidSphereIntersectsFixedSolidBox(s, f));
+    }
+
+    {
+        
+        Vector3 v0(0, 0, 0);
+        Vector3 v1(0, 0, -1);
+        Vector3 v2(-1, 0, 0);
+        Ray ray = Ray::fromOriginAndDirection(Vector3(-.15, 1,-.15), Vector3(0, -1, 0));
+        Vector3 location, normal;
+        double t = ray.intersectionTime(v0, v1, v2);
+        debugAssert(t == 1.0);
+
+        ray.origin.y = -ray.origin.y;
+        t = ray.intersectionTime(v0, v1, v2);
+        debugAssert(t == inf());
+
+        // One-sided test
+        ray.direction.y = -ray.direction.y;
+        ray.origin.y = -ray.origin.y;
+        t = ray.intersectionTime(v0, v1, v2);
+        debugAssert(t == inf());
+
+        // Time scale
+        ray = Ray::fromOriginAndDirection(Vector3(-.15, 1,-.15), Vector3(0, -2, 0));
+        t = ray.intersectionTime(v0, v1, v2);
+
+        debugAssert(t == 0.5);
+    }
+
+    printf("passed\n");
+}
---
+++ source/test/tMeshAlgTangentSpace.cpp
@@ -0,0 +1,46 @@
+#include "../include/G3DAll.h"
+
+void testMeshAlgTangentSpace() {
+    printf("MeshAlg::computeTangentSpaceBasis ");
+
+    MeshAlg::Geometry geometry;
+    Array<MeshAlg::Face> face;
+    Array<MeshAlg::Edge> edge;
+    Array<MeshAlg::Vertex> vertex;
+
+	Array<Vector2> texCoord;
+    Array<Vector3> tangent;
+    Array<Vector3> binormal;
+	Array<int>	   index;
+
+    // Create a quad
+	double s = 2;
+	geometry.vertexArray.append(Vector3(-s, -s, 0));
+	geometry.vertexArray.append(Vector3( s, -s, 0));
+	geometry.vertexArray.append(Vector3( s,  s, 0));
+	geometry.vertexArray.append(Vector3(-s,  s, 0));
+
+    texCoord.append(Vector2(0, 1));
+	texCoord.append(Vector2(1, 1));
+	texCoord.append(Vector2(1, 0));
+	texCoord.append(Vector2(0, 0));
+
+	index.append(0, 1, 2);
+	index.append(0, 2, 3);
+
+    MeshAlg::computeAdjacency(geometry.vertexArray, index, face, edge, vertex);
+    MeshAlg::computeNormals(geometry, index);
+
+    for (int i = 0; i < 4; ++i) {
+        debugAssert(geometry.normalArray[i].fuzzyEq(Vector3::unitZ()));
+    }
+
+    MeshAlg::computeTangentSpaceBasis(geometry.vertexArray, texCoord, geometry.normalArray, face, tangent, binormal);
+
+    for (int i = 0; i < 4; ++i) {
+        debugAssert(tangent[i].fuzzyEq(Vector3::unitX()));
+        debugAssert(binormal[i].fuzzyEq(-Vector3::unitY()));
+    }
+
+    printf("passed\n");
+}
