28d53a848cccaa340edf8bd922432249519d0fb3
morgan3d <morgan3d>
2005-09-07 00:31:02 +0000

    cube map bug test

--- source/Debug/main.cpp
+++ source/Debug/main.cpp
@@ -440,6 +440,9 @@ bool hasBuggyCubeMapTexCoords() {
 
         glTexParameteri(GL_TEXTURE_CUBE_MAP_ARB, GL_TEXTURE_MAG_FILTER, GL_NEAREST); 
         glTexParameteri(GL_TEXTURE_CUBE_MAP_ARB, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP_ARB, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP_ARB, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP_ARB, GL_TEXTURE_WRAP_R, GL_REPEAT);
     }
     
 
