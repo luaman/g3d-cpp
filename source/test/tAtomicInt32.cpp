1b149eb69043c02bd8679c58dd5d2c3bdea60d09
morgan3d <morgan3d>
2005-10-24 19:04:12 +0000

    surpressed warnings in Queue

--- source/test/tAtomicInt32.cpp
+++ source/test/tAtomicInt32.cpp
@@ -2,5 +2,25 @@
 
 void testAtomicInt32() {
     
+    {
+        AtomicInt32 a(1);
+        debugAssert(a.value() == 1);
+
+        a.increment();
+        debugAssert(a.value() == 2);
+
+        a.decrement();
+        debugAssert(a.value() == 1);
+
+        a = 10;
+        debugAssert(a.value() == 10);
+
+        a.compareAndSet(10, 1);
+        debugAssert(a.value() == 1);
+
+        a.compareAndSet(10, 15);
+        debugAssert(a.value() == 1);
+    }
+
 }
 
