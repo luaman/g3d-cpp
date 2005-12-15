dd4bf23135aa929af6765ff35b056273da79e4fc
ckodonnell <ckodonnell>
2005-12-15 20:45:59 +0000

    Changed sprintf format options on Mac OS X. Format is %x.%x.%x not %d.%d.%d.

--- source/G3Dcpp/System.cpp
+++ source/G3Dcpp/System.cpp
@@ -465,7 +465,7 @@ void System::init() {
         int minor = (macVersion >> 4) & 0xF;
         int revision = macVersion & 0xF;
 
-        sprintf(_operatingSystemCstr, "OS X %d.%d.%d", major, minor, revision); 
+        sprintf(_operatingSystemCstr, "OS X %x.%x.%x", major, minor, revision); 
                  
         //Clock Cycle Timing Information:
         Gestalt('pclk', &System::m_OSXCPUSpeed);
