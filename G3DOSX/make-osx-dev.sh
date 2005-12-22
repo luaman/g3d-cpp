ab1aebdfb0ee00a4d93289c8e1fdbaf176fa5d66
ckodonnell <ckodonnell>
2005-12-22 19:16:11 +0000

    Adding two sample shell scripts which build G3DTest, just for simplicity sake.

---
+++ G3DOSX/make-osx-dev.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Build G3D libraries in Development configuration 
+#
+# Script maintained by Casey O'Donnell (ckodonnell at users dot sourceforge dot net)
+#
+
+XCODE_PROJECT=G3DOSX.xcodeproj
+
+# Build the Development (aka debug) build style
+xcodebuild -project $XCODE_PROJECT -target G3DTest -configuration Development
+
